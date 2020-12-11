07afd82561f0f0b0bc8a7c73cf3cf4ee1ab3fc49
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-12-11 20:07:29 +0100

    added connectDB

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-
 const app = express();
+
+require("dotenv").config();
+const connectDB = require("./models/connectDB");
 
 const router = require("./routes/router");
 
 const path = 5000;
-
+connectDB();
 app.use("/api", router);
-
 app.listen(path, ()=> {
     console.log(`I'm listening on port ${path}`)
 });
