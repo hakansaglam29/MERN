08b35f49f119fd4ebb7aa33c91152bd4dfb14a1e
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-12-12 16:45:49 +0100

    added email checked

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,16 +1,27 @@
 const User = require("../models/UserModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");  // crypto method added
 exports.authRegister = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
+
   // TODO1: Validate the fields
-  // TODO2: check already registered
-  const salt = await bcrypt.genSalt(10);
-  const newPassword = await bcrypt.hash(password, salt);
+
+  // TODO2: check already registered (email)
+  const userData =  await User.findOne({ email });
+
+  if (userData) {
+    return res.status(400).json({error: [{message: "user already exist !!"}]});
+  }
+
+  const salt = await bcrypt.genSalt(10);  // crypto method added
+  const newPassword = await bcrypt.hash(password, salt); // crypto method added
+
+
+
   const user = new User({
     firstName,
     lastName,
     email,
-    password: newPassword, //crypted password
+    password: newPassword, // crypto method added
   });
   await user.save();
   //TODO: Error handling for saving
