0fd4806f217bb7ea8f9c8c34210bc564270d59ce
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-12-12 16:24:03 +0100

    added crypt password and send to DB

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,20 +1,23 @@
-exports.authRegister = (req,res) => {
-    // TODO: Register function!
-    const {firstName, lastName, email, password} = req.body;
-    console.log(req.body)
-    console.log(
-        "Fields:",
-        firstName,
-        lastName,
-        email,
-        password,
-    );
-    res.send("Register Completed");
+const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+exports.authRegister = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  // TODO1: Validate the fields
+  // TODO2: check already registered
+  const salt = await bcrypt.genSalt(10);
+  const newPassword = await bcrypt.hash(password, salt);
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password: newPassword, //crypted password
+  });
+  await user.save();
+  //TODO: Error handling for saving
+  res.send("Register Completed.");
 };
-
-exports.authLogin = (req,res) => {
-      // TODO : Auth.
-    // TODO : Login func.
-    console.log("Login Completed");
-    res.send("Login Completed");
+exports.authLogin = (req, res) => {
+  // TODO: Auth.
+  // TODO: Login func.
+  res.send("Login Completed");
 };
--- models/connectDB.js
+++ models/connectDB.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
         console.log("Succesfully connected to DB");
     } catch (error){
